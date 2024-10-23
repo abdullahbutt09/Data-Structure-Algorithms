ea951aa5ac0e0542db3c137df5df02f02b73f9b0
Abdullah <codewithbug@gmail.com>
2024-10-23 10:37:08 +0500

    Adding some comments in linked list files

--- Cpp-DSA/Linked-List/Linked_List_01.cpp
+++ Cpp-DSA/Linked-List/Linked_List_01.cpp
@@ -19,24 +19,25 @@ void LinkedListTraversal(struct Node* ptr)
 
 int main()
 {
+    //creating nodes
     Node *head = new Node();
     Node *one = new Node();
     Node *two = new Node();
     Node *three = new Node();
 
-    head->data = 45;
-    head->next = one;
+    head->data = 45; //storing data in linked list
+    head->next = one; //storing the address of next node element
 
-    one->data = 109;
-    one->next = two;
+    one->data = 109; 
+    one->next = two; 
 
-    two->data = 245;
-    two->next = three;
+    two->data = 245; 
+    two->next = three; 
 
     three->data = 56;
-    three->next = NULL;
+    three->next = NULL; //NULL indicates that the list END!
 
-    LinkedListTraversal(head);
+    LinkedListTraversal(head); //Function for printing the stored elements in linked-list by giving head to it and traversing to the NULL means -> head != NULL;
 
     return 0;
 }
