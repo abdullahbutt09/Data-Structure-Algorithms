066062bffce8712c395ff8173b5e1c1e12858f83
Abdullah <codewithbug@gmail.com>
2024-10-25 23:43:12 +0500

    .

--- Cpp-DSA/Linked-List/Linked_List_06.cpp
+++ Cpp-DSA/Linked-List/Linked_List_06.cpp
@@ -1,8 +1,43 @@
 //linked list deletion.
 #include <iostream>
 using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        cout << ptr->data << endl;
+        ptr = ptr->next;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!";
+    // creating nodes
+    Node *head = new Node();
+    Node *one = new Node();
+    Node *two = new Node();
+    Node *three = new Node();
+
+    head->data = 45;  // storing data in linked list
+    head->next = one; // storing the address of next node element
+
+    one->data = 109;
+    one->next = two;
+
+    two->data = 245;
+    two->next = three;
+
+    three->data = 56;
+    three->next = NULL; // NULL indicates that the list END!
+
+    LinkedListTraversal(head); // Function for printing the stored elements in linked-list by giving head to it and traversing to the NULL means -> head != NULL;
+
     return 0;
 }
