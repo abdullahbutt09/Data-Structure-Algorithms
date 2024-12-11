e74e0d57d1b3cbe996425b6afb01fb6eddc7009e
alpha <abdullah777butt@gmail.com>
2024-12-11 23:07:27 +0500

    adding post order traversal code

--- Cpp-DSA/Trees-Problems/Tree-Post-Order-Traversal/Binary_Tree_Post_Order_Traversal.cpp
+++ Cpp-DSA/Trees-Problems/Tree-Post-Order-Traversal/Binary_Tree_Post_Order_Traversal.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 struct tree
@@ -17,33 +17,32 @@ tree *CreateNode(int data)
     return NewNode;
 }
 
-void PreOrderTraversal(struct tree *root)
-{
-    if (root != nullptr)
-    {
+void PostOrderTraversal(struct tree * root){
+    if(root != nullptr){
+        PostOrderTraversal(root->left);
+        PostOrderTraversal(root->right);
         cout << root->data << " ";
-        PreOrderTraversal(root->left);
-        PreOrderTraversal(root->right);
     }
 }
 
-int main()
-{
-    tree *NewNode1 = CreateNode(1);
-    tree *NewNode2 = CreateNode(2);
-    tree *NewNode3 = CreateNode(3);
-    tree *NewNode4 = CreateNode(4);
-    tree *NewNode5 = CreateNode(5);
-    tree *NewNode6 = CreateNode(6);
-    tree *NewNode7 = CreateNode(7);
+int main(){
+    tree *NewNode1 = CreateNode(4);
+    tree *NewNode2 = CreateNode(1);
+    tree *NewNode3 = CreateNode(6);
+    tree *NewNode4 = CreateNode(5);
+    tree *NewNode5 = CreateNode(2);
+    tree *NewNode6 = CreateNode(7);
+    tree *NewNode7 = CreateNode(8);
 
     NewNode1->left = NewNode2;
     NewNode1->right = NewNode3;
+
     NewNode2->left = NewNode4;
     NewNode2->right = NewNode5;
+
     NewNode3->left = NewNode6;
     NewNode3->right = NewNode7;
 
-    PreOrderTraversal(NewNode1);
+    PostOrderTraversal(NewNode1);
     return 0;
 }
